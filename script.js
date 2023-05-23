e8a8f94b4964ce28d87a52deea7a5dc8405a6919
Nazareno Tarchini <ntarchini.developer@gmail.com>
2023-05-23 15:59:45 -0300

    cambios en portfolio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
+    let opciones = document.querySelectorAll('#links  a');
     opciones[0].className = "";
     opciones[1].className = "";
     opciones[2].className = "";
@@ -10,13 +10,13 @@ function seleccionar(link) {
 
     //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
     //en modo responsive
-    var x = document.getElementById("nav");
+    let x = document.getElementById("nav");
     x.className = "";
 }
 
 //función que muestra el menu responsive
 function responsiveMenu() {
-    var x = document.getElementById("nav");
+    let x = document.getElementById("nav");
     if (x.className === "") {
         x.className = "responsive";
     } else {
